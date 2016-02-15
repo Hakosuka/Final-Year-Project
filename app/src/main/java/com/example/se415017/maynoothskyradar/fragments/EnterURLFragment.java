de6f6c7b7bcc8f88aac74992b2da86fe50438258
se415017 <ciarancumiskey18594@gmail.com>
2016-02-15 17:57:33 +0000

    Edited EnterURLFragment so that it now has EditText fields for the user to enter their geographical coordinates.

--- app/src/main/java/com/example/se415017/maynoothskyradar/fragments/EnterURLFragment.java
+++ app/src/main/java/com/example/se415017/maynoothskyradar/fragments/EnterURLFragment.java
@@ -44,16 +44,24 @@ public class EnterURLFragment extends Fragment {
     // TODO: Rename and change types of parameters
     final String PREFS = "UserPreferences";
     final String SERVER_PREF = "serverAddress";
+    final String LAT_PREF = "latitude";
+    final String LON_PREF = "longitude";
     final String TAG = getClass().getSimpleName();
 
     @Bind(R.id.edit_url_fragment_title)
     TextView editUrlFragmentTitle;
     @Bind(R.id.edit_url_fragment_content)
     TextView editUrlFragmentContent;
+    @Bind(R.id.edit_lat_lon_content)
+    TextView editLatLonContent;
 
     @Bind(R.id.server_address_edit_field)
     EditText serverAddressEditor;
     //TODO: Maybe it would be a good idea to ask the user to enter their latitude and longitude
+    @Bind(R.id.latitude_edit_field)
+    EditText latitudeEditor;
+    @Bind(R.id.longitude_edit_field)
+    EditText longitudeEditor;
 
     @Bind(R.id.button_submit_server_address)
     Button submitServerAddressButton;
@@ -111,9 +119,28 @@ public class EnterURLFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                clearServerAddressEditorButton.setEnabled(s.toString().length() > 0);
+                Log.d("TAG", "Entered URL = " + s);
+                clearServerAddressEditorButton.setEnabled(s.length() > 0);
                 //TODO: Add a RegEx check within setEnabled() below
-                submitServerAddressButton.setEnabled(s.toString().length() > 7);
+                submitServerAddressButton.setEnabled(s.length() > 7);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        latitudeEditor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.d(TAG, "Entered latitude = " + s);
+                clearServerAddressEditorButton.setEnabled(s.length() > 0);
+                submitServerAddressButton.setEnabled();
             }
 
             @Override
@@ -138,7 +165,14 @@ public class EnterURLFragment extends Fragment {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         String editorText = serverAddressEditor.getText().toString();
+        // Android doesn't have a "putDouble()" method - thanks for being lazy, Google!
+        long latFromEditor = Double.doubleToRawLongBits(
+                Double.parseDouble(latitudeEditor.getText().toString()));
+        long lonFromEditor = Double.doubleToRawLongBits(
+                Double.parseDouble(longitudeEditor.getText().toString()));
         editor.putString(SERVER_PREF, editorText);
+        editor.putFloat(LAT_PREF, latFromEditor);
+        editor.putLong(LON_PREF, lonFromEditor);
         editor.apply();
         Intent returnToMainActivityIntent = new Intent(getActivity(), MainActivity.class);
         startActivity(returnToMainActivityIntent);
@@ -147,11 +181,31 @@ public class EnterURLFragment extends Fragment {
     @OnClick(R.id.button_clear)
     protected void resetURLField(){
         serverAddressEditor.setText("");
+        latitudeEditor.setText("");
+        longitudeEditor.setText("");
         enableResetButton(false);
         enableSubmitButton(false);
     }
 
+    /**
+     *
+     * @param lat the string to be checked to ensure that it's a number between -90 and 90 (inclusive)
+     * @return whether or not the entered number is between -90 and 90
+     */
+    public boolean checkForValidLat(String lat){
+        double latDouble = Double.parseDouble(lat);
+        return -90.0 <= latDouble && latDouble <= 90.0;
+    }
 
+    /**
+     *
+     * @param lon the string to be entered to ensure that it's a number between -180 and 180 (inclusive)
+     *
+     */
+    public boolean checkForValidLon(String lon){
+        double lonDouble = Double.parseDouble(lon);
+        return -180.0 <= lonDouble && lonDouble <= 180.0;
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
--- app/src/main/java/com/example/se415017/maynoothskyradar/objects/aircraft.java
+++ app/src/main/java/com/example/se415017/maynoothskyradar/objects/aircraft.java
@@ -11,13 +11,28 @@ public class Aircraft extends RealmObject {
     @PrimaryKey private String icaoHexAddr;
     //String flightNum; //Not much point in this, an aircraft would do multiple flight routes
     private String regCode = "TBD"; // registration code of the plane, I'll probably fetch this from a database
-    /* private int altitude;
-    private int speed;
-    private int track;
-    private double lat, lon;*/
+    private int altitude;
+    /*private int speed;
+    private int track;*/
+    private double lat, lon;
 
     public String getIcaoHexAddr() { return icaoHexAddr; }
     public void setIcaoHexAddr(String icaoHexAddr) { this.icaoHexAddr = icaoHexAddr; }
+
     public String getRegCode() { return regCode; }
     public void setRegCode(String regCode) { this.regCode = regCode; }
+
+    public double[] getLocation(){
+        double[] latAndLon = {lat, lon};
+        return latAndLon;
+    }
+    public void setLocation(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public int getAltitude() { return altitude; }
+    public void setAltitude(int altitude){
+        this.altitude = altitude;
+    }
 }
