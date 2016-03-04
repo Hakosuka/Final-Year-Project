0fa3b65d5dbdd58532eca4f40cbfc95a6290aee5
ciaranc94 <ciarancumiskey18594@gmail.com>
2016-03-04 21:31:50 +0000

    Worked on the RecylerViewAdapter and its corresponding ListFragment, and the AircraftListItem that'd be needed to go along with it.

--- app/src/main/java/com/example/se415017/maynoothskyradar/fragments/AircraftRecyclerViewAdapter.java
+++ app/src/main/java/com/example/se415017/maynoothskyradar/fragments/AircraftRecyclerViewAdapter.java
@@ -3,38 +3,55 @@ package com.example.se415017.maynoothskyradar.fragments;
 import android.content.Context;
 import android.support.v4.content.res.TypedArrayUtils;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.se415017.maynoothskyradar.R;
-//TODO: Implement THIS
-import com.example.se415017.maynoothskyradar.fragments.AircraftListItem.OnListFragmentInteractionListener;
+//TODO: Check if I need to implement THIS
+//import com.example.se415017.maynoothskyradar.fragments.AircraftListItem.OnListFragmentInteractionListener;
 import com.example.se415017.maynoothskyradar.fragments.dummy.DummyContent.DummyItem;
 import com.example.se415017.maynoothskyradar.objects.Aircraft;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
- * specified {@link OnListFragmentInteractionListener}.
+ * specified OnListFragmentInteractionListener}.
  * TODO: Replace the implementation with code for your data type.
  */
 public class AircraftRecyclerViewAdapter extends
         RecyclerView.Adapter<AircraftRecyclerViewAdapter.ViewHolder> {
 
     private ArrayList<Aircraft> aircraftList;
-    private final AircraftListFragment.OnListFragmentInteractionListener mListener;
+    private List<AircraftListItem> aircraftListItems;
+    //private final AircraftListFragment.OnListFragmentInteractionListener mListener;
 
     private Context context;
     private boolean useList = true;
 
     public AircraftRecyclerViewAdapter(Context context, List<Aircraft> items) {
         this.context = context;
-        this.aircraftList = (ArrayList) items;
-        mListener = listener;
+        if (items != null) {
+            if(items.size() > 0) {
+                for (int i = 0; i < items.size(); i++) {
+                    Object unknownTypeObject = items.get(i);
+                    if(unknownTypeObject != null){
+                        Aircraft aircraftToShow = (Aircraft) unknownTypeObject;
+                        aircraftList.add(aircraftToShow);
+                        aircraftListItems.add(new AircraftListItem(aircraftToShow));
+                    }
+                }
+            }
+        }
+        //mListener = listener;
     }
 
     @Override
@@ -47,19 +64,11 @@ public class AircraftRecyclerViewAdapter extends
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = aircraftList.get(position);
-        holder.mIdView.setText(aircraftList.get(position).icaoHexAddr);
-        holder.mContentView.setText(aircraftList.get(position).getPosString());
-
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
-                }
-            }
-        });
+        holder.hexContent.setText(aircraftList.get(position).icaoHexAddr);
+        holder.altitudeContent.setText(aircraftList.get(position).altitude);
+        holder.latitudeContent.setText(aircraftList.get(position).latitude);
+        holder.longitudeContent.setText(aircraftList.get(position).longitude);
+        holder.callsignContent.setText(aircraftList.get(position).callsign);
     }
 
     @Override
@@ -67,22 +76,33 @@ public class AircraftRecyclerViewAdapter extends
         return aircraftList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @Bind(R.id.hex_title) TextView hexTitle;
+        @Bind(R.id.hex_content) TextView hexContent;
+        @Bind(R.id.callsign_content) TextView callsignContent;
+        @Bind(R.id.altitude_title) TextView altitudeTitle;
+        @Bind(R.id.altitude_content) TextView altitudeContent;
+        @Bind(R.id.latitude_title) TextView latitudeTitle;
+        @Bind(R.id.latitude_content) TextView latitudeContent;
+        @Bind(R.id.longitude_title) TextView longitudeTitle;
+        @Bind(R.id.longitude_content) TextView longitudeContent;
         public Aircraft mItem;
 
         public ViewHolder(View view) {
             super(view);
-            mView = view;
-            mIdView = (TextView) view.findViewById(R.id.id);
-            mContentView = (TextView) view.findViewById(R.id.content);
+            ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
         }
 
         @Override
         public String toString() {
-            return super.toString() + " '" + mContentView.getText() + "'";
+            return super.toString() + " '" + hexContent.getText() + "'";
+        }
+
+        @Override
+        public void onClick(View view){
+            Log.d("AircraftRecycler", view.toString());
         }
     }
 }
